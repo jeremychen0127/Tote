6d5df85cf70a28cb5ff3febe3a3722afea450402
mding5692 <m.ding5692@gmail.com>
2016-06-11 16:56:55 -0400

    Adds clarifai image recog http calls

--- server/main.js
+++ server/main.js
@@ -1,6 +1,6 @@
 import { Meteor } from 'meteor/meteor';
-var Clarifai = require('clarifai');
-var Haveondemand = require('havenondemand');
+/*var Clarifai = require('clarifai');
+var Havenondemand = require('havenondemand');*/
 
 Meteor.startup(() => {
   // API Keys
@@ -8,12 +8,59 @@ Meteor.startup(() => {
   var clarifaiID = "WfikzkWvhIFgs7BXKq01pNeYyklRTCPELYqNcwBF";
   var clarifaiSecret = "eJn_t3-WfzeGY37diKxTruPmvlN-OB--zuPzsPLn";
   var clarifaiToken = "f6xSge2rF91T25y16VNOwLgoZbz7PB";
+  var clarifaiBaseUrl = "https://api.clarifai.com/v1/";
 
-  var hpeCLient = new havenondemand.HODClient(hpeAPIKey);
+  var clarifaiTokenData = {
+    grant_type: 'client_credentials',
+    client_id: clarifaiID,
+    client_secret: clarifaiSecret
+  };
+
+  var imageUrl = "http://gloimg.rosewholesale.com/ROSE/2014/201408/goods-img/1407522188865-P-1924922.jpg";
+
+  getClarifaiToken();
+  getTagsFromUrl(imageUrl,clarifaiToken);
+
+
+
+  /* Functions */
+
+  function getClarifaiToken() {
+    clarifaiBaseUrl += "token";
+    HTTP.call('POST', clarifaiBaseUrl, {"data": clarifaiTokenData }, function(err,res) {
+      if (err) {
+        console.log("Error occurred " + err);
+      } else {
+        clarifaiToken = res;
+      }
+    });
+
+  }
+
+  function getTagsFromUrl(imgUrl, accessToken) {
+    clarifaiBaseUrl += "tag";
+
+    var data = {
+      "url" : imgUrl
+    };
+    var headers = {
+      "Authorization": 'Bearer '+ accessToken
+    }
+    console.log("url: " + imgUrl);
+
+    HTTP.call("POST", clarifaiBaseUrl, {"data": data, "headers": headers }, function(err,res) {
+      if (err) {
+        console.log("Error: " + err);
+      } else {
+        console.log(res);
+      }
+    });
+  }
+/*  var hpeCLient = new Havenondemand.HODClient(hpeAPIKey);
 
   var clarifaiClient = new Clarifai({
-    "id": clarifaiID,
-    "secret": clarifaiSecret
+    id: clarifaiID,
+    secret: clarifaiSecret
   });
 
   // test image url
@@ -25,5 +72,7 @@ Meteor.startup(() => {
     } else {
       console.log(results)
     }
-  })
+  }) */
+
+
 });
