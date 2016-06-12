dd4c0da11ce344b472c9e4b33ffb6070fb7a13ff
Philip Chang <lichung.c@gmail.com>
2016-06-12 07:23:07 -0400

    added fashionItem methods

--- server/main.js
+++ server/main.js
@@ -1,3 +1,6 @@
 import '../imports/api/users.js';
 import '../imports/api/userProfile.js';
 import '../imports/api/mock_data_import';
+import '../imports/api/havenondemand';
+import '../imports/api/clarifai';
+import '../imports/api/fashionItem';
