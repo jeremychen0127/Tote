fe4a5fa6602cb8f47cd741a4f4efdc78617d0869
Jeremy Chen <jeremychen0127@gmail.com>
2016-06-12 09:40:26 -0400

    Added working version of closet

--- imports/ui/pages/Closet.jsx
+++ imports/ui/pages/Closet.jsx
@@ -13,6 +13,8 @@ import MoreVertIcon from 'material-ui/svg-icons/navigation/more-vert';
 import RaisedButton from 'material-ui/RaisedButton';
 import DropDownMenu from 'material-ui/DropDownMenu';
 import { createContainer } from 'meteor/react-meteor-data';
+import Divider from 'material-ui/Divider';
+
 
 import {FashionItemCollection} from '../../api/fashionItem';
 
@@ -30,29 +32,6 @@ const styles = {
   },
 };
 
-const tilesData = [
-  {
-    img: '/img/logo.png',
-    title: 'Breakfast',
-  },
-  {
-    img: '/img/Hardwood.jpg',
-    title: 'Breakfast',
-  },
-    {
-      img: '/img/black.jpg',
-      title: 'Breakfast',
-    },
-   {
-        img: '/img/woodenfloor.jpg',
-        title: 'Breakfast',
-   },
-   {
-        img: '/img/black.jpg',
-        title: 'Breakfast',
-   },
-];
-
 export default class Closet extends Component {
   constructor(props) {
     super(props);
@@ -61,6 +40,7 @@ export default class Closet extends Component {
       photoCategory: '',
       topUrl: '',
       bottomUrl: '',
+      shoesUrl: '',
     };
 
     this.handleGridListChange = (event, index, value) => this.setState({gridListCategory: value});
@@ -107,23 +87,29 @@ export default class Closet extends Component {
       this.setState({topUrl: event.target.value.itemUrl});
     } else if (event.target.value.category === 'bottoms') {
       this.setState({bottomUrl: event.target.value.itemUrl});
+    } else if (event.target.value.category === 'shoes') {
+      this.setState({shoesUrl: event.target.value.itemUrl});
     }
   }
 
   renderGridList() {
     var _this = this;
     var filteredItems = this.props.fashionItems.filter((item) => {
-      return item.category === _this.state.gridListCategory;
+      if (_this.state.gridListCategory !== 'all') {
+        return item.category === _this.state.gridListCategory;
+      } else {
+        return true;
+      }
     });
     return filteredItems.map((item) => {
       return (
         <GridTile
           key={item.itemUrl}
           value={item}
-          title=' '
-          actionIcon={<IconButton onClick={_this.handleStarClick.bind(_this)}><StarBorder color="red" /></IconButton>}
+          onClick={_this.handleStarClick.bind(_this)}
+          style={{cursor: 'pointer'}}
         >
-          <img src={item.itemUrl} />
+          <img value={item} src={item.itemUrl} />
         </GridTile>
       );
     });
@@ -139,7 +125,6 @@ export default class Closet extends Component {
                 anchorOrigin={{horizontal: 'middle', vertical: 'top'}}
                 targetOrigin={{horizontal: 'middle', vertical: 'top'}}
               >
-                <MenuItem value="all" primaryText="All" />
                 <MenuItem value="tops" primaryText="Tops" />
                 <MenuItem value="bottoms" primaryText="Bottoms" />
                 <MenuItem value="dresses" primaryText="Dresses" />
@@ -153,24 +138,27 @@ export default class Closet extends Component {
                 </div>
             </form>
           </div>
-          <div className="row">
-            <div className="col m8">
+          <Divider />
+          <br />
+          <div className="row" style={{marginBottom: '0px'}}>
+            <div className="col m7">
               <div className="center">
                 {
                   this.state.topUrl ?
-                    <img src={this.state.topUrl} />
+                    <Paper zDepth={2}>
+                      <img height={300} width={300} src={this.state.topUrl} />
+                    </Paper>
                   :
-                    <Paper />
+                    <Paper zDepth={0}>
+                      <br /><br /><br />
+                    </Paper>
                 }
               </div>
             </div>
-            <div className="col m4">
-              <div style={styles.root}>
-                <GridList
-                  cellHeight={200}
-                  style={styles.gridList}
-                >
-                  <Subheader className="center">
+            <div className="col offset-m1 m4">
+              <div >
+                <GridList>
+                  <Subheader className="center" style={{height: '50px'}}>
                     <DropDownMenu value={this.state.gridListCategory} onChange={this.handleGridListChange}
                       anchorOrigin={{horizontal: 'middle', vertical: 'top'}}
                       targetOrigin={{horizontal: 'middle', vertical: 'top'}}
@@ -185,6 +173,38 @@ export default class Closet extends Component {
                   {this.renderGridList()}
 
                 </GridList>
+              </div>
+            </div>
+          </div>
+          <div className="row" style={{marginBottom: '0px'}}>
+            <div className="col m7">
+              <div className="center">
+                {
+                  this.state.bottomUrl ?
+                    <Paper zDepth={2}>
+                      <img height={300} width={300} src={this.state.bottomUrl} />
+                    </Paper>
+                  :
+                    <Paper zDepth={0}>
+                      <br /><br /><br />
+                    </Paper>
+                }
+              </div>
+            </div>
+          </div>
+          <div className="row">
+            <div className="col m7">
+              <div className="center">
+                {
+                  this.state.shoesUrl ?
+                    <Paper zDepth={2}>
+                      <img height={300} width={300} src={this.state.shoesUrl} />
+                    </Paper>
+                  :
+                    <Paper zDepth={0}>
+                      <br /><br /><br />
+                    </Paper>
+                }
               </div>
             </div>
           </div>
