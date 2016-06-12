5b62fff10a15b76c06456cdf592aa58bf978cf8a
Jeremy Chen <jeremychen0127@gmail.com>
2016-06-11 21:02:30 -0400

    Added login system

--- imports/ui/components/Logo.jsx
+++ imports/ui/components/Logo.jsx
@@ -1,4 +1,7 @@
 import React, { Component } from 'react';
+import Paper from 'material-ui/Paper';
+import AccountsUIWrapper from './accounts/AccountsUIWrapper.jsx';
+import { createContainer } from 'meteor/react-meteor-data';
 
 export default class Logo extends Component {
   render() {
@@ -7,7 +10,19 @@ export default class Logo extends Component {
         <div className="col m8 offset-m2 s12">
           <img src="/img/logo.png" height="100"/>
         </div>
+        {
+          this.props.currentUserId ?
+            <div className="col m2">
+              Hi, <AccountsUIWrapper />
+            </div>
+          :
+            ''
+        }
       </div>
     );
   }
 }
+
+export default createContainer(() => {
+  return { currentUserId: Meteor.userId() };
+}, Logo);
--- imports/ui/pages/Welcome.jsx
+++ imports/ui/pages/Welcome.jsx
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import Paper from 'material-ui/Paper';
 import baseTheme from 'material-ui/styles/baseThemes/lightBaseTheme';
 import getMuiTheme from 'material-ui/styles/getMuiTheme';
+import AccountsUIWrapper from '../components/accounts/AccountsUIWrapper.jsx';
+import { createContainer } from 'meteor/react-meteor-data';
 import { FlowRouter } from 'meteor/kadira:flow-router';
 
 const styles = {
@@ -10,11 +12,11 @@ const styles = {
   }
 };
 
-export default class Welcome extends Component {
+class Welcome extends Component {
   constructor(props) {
     super(props);
     this.handleSelectGender = (event) => {
-//      Meteor.call('tote.userProfile.updateGender', gender);
+      Meteor.call('tote.userProfile.updateGender', event.target.value);
       FlowRouter.go('recommendations');
     }
   }
@@ -25,21 +27,32 @@ export default class Welcome extends Component {
 
   render() {
     return (
-      <div className="row">
-        <div className="col offset-m1 m4 s12 hoverable center genderButton" style={styles.genderButton}>
-          <Paper value="male" onClick={this.handleSelectGender}>
-            <br /><br />
-            <h2>Male</h2>
-            <br /><br />
-          </Paper>
-        </div>
-        <div className="col offset-m2 m4 s12 hoverable center genderButton" style={styles.genderButton}>
-          <Paper value="female" onClick={this.handleSelectGender}>
-            <br /><br />
-            <h2>Female</h2>
-            <br /><br />
-          </Paper>
-        </div>
+      <div>
+        { !this.props.currentUserId ?
+            <div className="center">
+              <AccountsUIWrapper />
+            </div>
+          :
+            <div>
+              <br />
+              <div className="row">
+                <div className="col offset-m1 m4 s12 hoverable center genderButton" style={styles.genderButton}>
+                  <Paper value="male" onClick={this.handleSelectGender}>
+                    <br /><br />
+                    <h2>Male</h2>
+                    <br /><br />
+                  </Paper>
+                </div>
+                <div className="col offset-m2 m4 s12 hoverable center genderButton" style={styles.genderButton}>
+                  <Paper value="female" onClick={this.handleSelectGender}>
+                    <br /><br />
+                    <h2>Female</h2>
+                    <br /><br />
+                  </Paper>
+                </div>
+              </div>
+            </div>
+        }
       </div>
     );
   }
@@ -48,3 +61,7 @@ export default class Welcome extends Component {
 Welcome.childContextTypes = {
   muiTheme: React.PropTypes.object.isRequired,
 };
+
+export default createContainer(() => {
+  return { currentUserId: Meteor.userId() };
+}, Welcome);
